39859b0a9cf87e859f70e235550b0947c4003068
JaeMeDev <jaeme0406@gmail.com>
2022-06-04 20:14:53 +0900

    feat: add main route

--- src/pages/manage_study.tsx
+++ src/pages/manage_study.tsx
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled';
 import MainLayout from '@components/layout/MainLayout';
 
-const Home = () => (
-  <MainLayout>
-    <Container>Home</Container>
+const ManageStudy = () => (
+  <MainLayout title="스터디 관리">
+    <Container>ManageStudy</Container>
   </MainLayout>
 );
 
 const Container = styled.div``;
 
-export default Home;
+export default ManageStudy;
--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled';
 import MainLayout from '@components/layout/MainLayout';
 
-const Home = () => (
-  <MainLayout>
-    <Container>Home</Container>
+const Search = () => (
+  <MainLayout title="검색">
+    <Container>Search</Container>
   </MainLayout>
 );
 
 const Container = styled.div``;
 
-export default Home;
+export default Search;
